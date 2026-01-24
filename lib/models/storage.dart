41faf4420478869b803eedc914b9f468979c3713
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2026-01-24 15:51:56 -0800

    initial hive

---
+++ lib/models/storage.dart
@@ -0,0 +1,4 @@
+library folicle.storage;
+import "package:hive/hive.dart";
+
+Box weeklyHistoryBox = Hive.box("delete_me");
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:folicle/screens/initial_assessment_screen.dart';
 import 'package:folicle/screens/weekly_checkin_screen.dart';
+import 'package:folicle/models/storage.dart' as storage;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   // TEMP: hardcoded for now
-  final bool isProfileComplete = false;
+  final bool isProfileComplete = true;
 
   @override
   Widget build(BuildContext context) {
+    //storage.weeklyHistoryBox.put('numDays', [0.9, 0.1, 0.5]);
     return Scaffold(
       appBar: AppBar(title: const Text('Folicle'), centerTitle: true),
       body: Center(
